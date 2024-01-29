b63ad5a1d1512f7d9710e8a4df46910e53fb1db2
Jessica Sweeney <jsweeney97@uchicago.edu>
2024-01-28 20:09:15 -0700

    fixed indent error in blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -26,20 +26,20 @@ def create():
         body = request.form['body']
         error = None
 
-    if not title:
-        error = 'Title is required.'
-    
-    if error is not None:
-        flash(error)
-    else:
-        db = get_db()
-        db.execute(
-            'INSERT INTO post (title, body, author_id)'
-            ' VALUES (?, ?, ?)',
-            (title, body, g.user['id'])
-        )
-        db.commit()
-        return redirect(url_for('blog.index'))
+        if not title:
+            error = 'Title is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
     
     return render_template('blog/create.html')
 
