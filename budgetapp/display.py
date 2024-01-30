c56338e1834cc3c1ec4df605317ea7cc33215ec7
Jessica Sweeney <jsweeney97@uchicago.edu>
2024-01-30 11:55:44 -0700

    add order by to display sql to always show most recent upload

--- budgetapp/display.py
+++ budgetapp/display.py
@@ -16,5 +16,6 @@ def display():
     data = db.execute(
         'SELECT descr, amount'
         ' FROM debit d JOIN user u ON d.user_id = u.id'
+        ' ORDER BY uploaded_at DESC'
     ).fetchmany(20)
     return render_template('display/display.html', data=data)
