759cdc77598bba6d5f3e575a81e546c091c297c9
Jessica Sweeney <jsweeney97@uchicago.edu>
2024-02-26 10:13:46 -0700

    add merchants_quarantine and weekly_quarters to schema

--- budgetapp/schema.sql
+++ budgetapp/schema.sql
@@ -5,6 +5,8 @@ DROP TABLE IF EXISTS transaction_quarantine;
 DROP TABLE IF EXISTS merchants;
 DROP TABLE IF EXISTS travel;
 DROP TABLE IF EXISTS budget;
+DROP TABLE IF EXISTS merchants_quarantine;
+DROP TABLE IF EXISTS weekly_quarters;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,7 +25,7 @@ CREATE TABLE debit (
   parsed_descr TEXT NOT NULL,
   amount REAL NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user (id),
-  FOREIGN KEY (descr) REFERENCES merchant_to_category (merchant)
+  FOREIGN KEY (descr) REFERENCES merchants (merchant)
 );
 
 CREATE TABLE credit (
@@ -57,6 +59,14 @@ CREATE TABLE merchants (
   uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE merchants_quarantine (
+  merchant TEXT,
+  category TEXT,
+  information TEXT,
+  tags TEXT,
+  uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE travel (
   day_of DATE NOT NULL,
   user_id INTEGER NOT NULL,
@@ -71,3 +81,42 @@ CREATE TABLE budget (
     dollar_limit REAL NOT NULL,
     time_period TEXT DEFAULT 'Monthly'
 );
+
+CREATE TABLE weekly_quarters (
+    day_of_month INTEGER,
+    quarter INTEGER
+);
+
+INSERT INTO weekly_quarters VALUES (1, 1);
+INSERT INTO weekly_quarters VALUES (2, 1);
+INSERT INTO weekly_quarters VALUES (3, 1);
+INSERT INTO weekly_quarters VALUES (4, 1);
+INSERT INTO weekly_quarters VALUES (5, 1);
+INSERT INTO weekly_quarters VALUES (6, 1);
+INSERT INTO weekly_quarters VALUES (7, 1);
+INSERT INTO weekly_quarters VALUES (8, 1);
+INSERT INTO weekly_quarters VALUES (9, 2);
+INSERT INTO weekly_quarters VALUES (10, 2);
+INSERT INTO weekly_quarters VALUES (11, 2);
+INSERT INTO weekly_quarters VALUES (12, 2);
+INSERT INTO weekly_quarters VALUES (13, 2);
+INSERT INTO weekly_quarters VALUES (14, 2);
+INSERT INTO weekly_quarters VALUES (15, 2);
+INSERT INTO weekly_quarters VALUES (16, 2);
+INSERT INTO weekly_quarters VALUES (17, 3);
+INSERT INTO weekly_quarters VALUES (18, 3);
+INSERT INTO weekly_quarters VALUES (19, 3);
+INSERT INTO weekly_quarters VALUES (20, 3);
+INSERT INTO weekly_quarters VALUES (21, 3);
+INSERT INTO weekly_quarters VALUES (22, 3);
+INSERT INTO weekly_quarters VALUES (23, 3);
+INSERT INTO weekly_quarters VALUES (24, 4);
+INSERT INTO weekly_quarters VALUES (25, 4);
+INSERT INTO weekly_quarters VALUES (26, 4);
+INSERT INTO weekly_quarters VALUES (27, 4);
+INSERT INTO weekly_quarters VALUES (28, 4);
+INSERT INTO weekly_quarters VALUES (29, 4);
+INSERT INTO weekly_quarters VALUES (30, 4);
+INSERT INTO weekly_quarters VALUES (31, 4);
+
+
